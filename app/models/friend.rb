b416a5160a18c90ce139b8050bf7af4e09508194
Astrid Bosch <boschschwarz@hotmail.com>
2018-11-12 16:03:16 -0300

    Adding   mount_uploader :picture, PhotoUploader

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -5,5 +5,7 @@ class Friend < ApplicationRecord
   validates :slogan, presence: true
   validates :price, presence: true
   validates :user_id, presence: true
+  mount_uploader :picture, PhotoUploader
+
 
 end
