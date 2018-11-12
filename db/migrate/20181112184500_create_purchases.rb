b0ed375e9a5765368099addd894d99a3a83f706d
Nicolas Schroeder <nicolas.2000@hotmail.de>
2018-11-12 18:33:43 -0300

    Seeding finished without pictures

--- db/migrate/20181112184500_create_purchases.rb
+++ db/migrate/20181112184500_create_purchases.rb
@@ -2,7 +2,6 @@ class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
       t.integer :total_price
-      t.integer :confirmation_number
       t.references :user, foreign_key: true
 
       t.timestamps
