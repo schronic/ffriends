ffa9657daa5f348634723d614744f2847cfa69cd
Tyson Hazard <tysonhazard@gmail.com>
2018-11-12 16:18:53 -0300

    Add views to friends, change index route.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,3 +1,25 @@
 class FriendsController < ApplicationController
 
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
