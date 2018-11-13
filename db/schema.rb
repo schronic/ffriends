86d318ee6fadba5dd4f8ad9f9f47bace78055805
Astrid Bosch <boschschwarz@hotmail.com>
2018-11-13 10:50:06 -0300

    chanign the schema of the database and taking out the confirmation number validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_185828) do
+ActiveRecord::Schema.define(version: 2018_11_12_215721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2018_11_12_185828) do
 
   create_table "purchases", force: :cascade do |t|
     t.integer "total_price"
-    t.integer "confirmation_number"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
