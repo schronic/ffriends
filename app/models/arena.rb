1c32a37e58bc85df9931af0cdcabba7296eb8dfd
Nicolas Schroeder <nicolas.2000@hotmail.de>
2018-11-16 17:19:34 -0300

    address

--- app/models/arena.rb
+++ app/models/arena.rb
@@ -5,5 +5,4 @@ class Arena < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :picture, PhotoUploader
-
 end
--- db/migrate/20181116172855_fix_tables.rb
+++ db/migrate/20181116172855_fix_tables.rb
@@ -2,7 +2,6 @@ class FixTables < ActiveRecord::Migration[5.2]
   def change
         remove_column :arenas, :capacity
         remove_column :arenas, :level
-        remove_column :arenas, :address
         remove_column :fights, :date
         drop_table :carts
   end
