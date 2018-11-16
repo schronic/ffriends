88908ff4cfae44f4412fee51057d8c3aece46434
Nicolas Schroeder <nicolas.2000@hotmail.de>
2018-11-16 16:36:18 -0300

    final seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,7 +69,7 @@ arenaa = Arena.new(
   arenaa.save!
 
 arenab = Arena.new(
-  name: '',
+  name: 'Tyria',
   latitude: 58.3816,
   langitude: 34.6037
   )
@@ -81,26 +81,22 @@ arenac = Arena.new(
   latitude: 40.7128,
   langitude: 74.0060
   )
-  arenacremote_picture_url =  Cloudinary::Uploader.upload('https://www.google.com.ar/search?biw=1299&bih=669&tbm=isch&sa=1&ei=MhDvW-TfJIKRwgSvgrXQBQ&q=meeren+got&oq=meeren+got&gs_l=img.3..0j0i24k1.1208.1964.0.2283.4.3.0.1.1.0.87.213.3.3.0....0...1c.1.64.img..0.4.216...0i5i30k1j0i10i24k1.0.49ekzBIYgCQ#imgrc=qkyq4tQdSUic-M:')['url']
-  arenacsave!
+  arenacremote_picture_url =  Cloudinary::Uploader.upload('https://2.bp.blogspot.com/-3djK1nxKrh8/WTSY0pJUMiI/AAAAAAAAS_M/ohLUIO9-kzA1wIyGCPtC68krEJfWmRpfACLcB/s1600/Game-of-Thrones-Kings-Landing.jpg')['url']
+  arenac.save!
 
 arenae = Arena.new(
   name: 'Lannisport',
   latitude: 36.7783,
   langitude: 119.4179
   )
-  arendaeremote_picture_url =  Cloudinary::Uploader.upload('https://www.google.com.ar/search?biw=1299&bih=669&tbm=isch&sa=1&ei=MhDvW-TfJIKRwgSvgrXQBQ&q=meeren+got&oq=meeren+got&gs_l=img.3..0j0i24k1.1208.1964.0.2283.4.3.0.1.1.0.87.213.3.3.0....0...1c.1.64.img..0.4.216...0i5i30k1j0i10i24k1.0.49ekzBIYgCQ#imgrc=qkyq4tQdSUic-M:')['url']
-  arendaesave!
+  arendaeremote_picture_url =  Cloudinary::Uploader.upload('https://vignette.wikia.nocookie.net/menirth/images/9/93/Port_little5.jpg/revision/latest?cb=20170728135612')['url']
+  arendae.save!
 
 arenad = Arena.new(
   name: 'Volantis',
   latitude: 31.0461,
   langitude: 34.8516
   )
-  arenadremote_picture_url =  Cloudinary::Uploader.upload('https://www.google.com.ar/search?biw=1299&bih=669&tbm=isch&sa=1&ei=MhDvW-TfJIKRwgSvgrXQBQ&q=meeren+got&oq=meeren+got&gs_l=img.3..0j0i24k1.1208.1964.0.2283.4.3.0.1.1.0.87.213.3.3.0....0...1c.1.64.img..0.4.216...0i5i30k1j0i10i24k1.0.49ekzBIYgCQ#imgrc=qkyq4tQdSUic-M:')['url']
-  arenadsave!
+  arenadremote_picture_url =  Cloudinary::Uploader.upload('https://www.google.com.ar/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwii-NPP0tneAhVJgZAKHcgYCY8QjRx6BAgBEAU&url=http%3A%2F%2Fhieloyfuego.wikia.com%2Fwiki%2FVolantis&psig=AOvVaw09f129MSEK3jVUGgFrfkpV&ust=1542482871493271')['url']
+  arenad.save!
 
-
-Volantis, Lannisport, Saatarh , Tyria
-
-Ooros
